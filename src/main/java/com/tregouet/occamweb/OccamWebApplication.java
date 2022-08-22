8469dd6b8ce1c812bd4b5894689d04fafe9f45d4
Gael Tregouet <TREGOUET@192.168.0.16>
2022-08-22 10:37:43 +0200

    strategy modif

--- src/main/java/com/tregouet/occamweb/OccamWebApplication.java
+++ src/main/java/com/tregouet/occamweb/OccamWebApplication.java
@@ -3,16 +3,17 @@ package com.tregouet.occamweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.tregouet.occam.Occam;
 import com.tregouet.occam.alg.OverallAbstractFactory;
 import com.tregouet.occam.alg.OverallStrategy;
 
 @SpringBootApplication
 public class OccamWebApplication {
 
-	public static final OverallStrategy strategy = OverallStrategy.OVERALL_STRATEGY_3;
+	public static final OverallStrategy STRATEGY = Occam.STRATEGY;
 
 	public static void initialize() {
-		OverallAbstractFactory.INSTANCE.apply(strategy);
+		OverallAbstractFactory.INSTANCE.apply(STRATEGY);
 	}
 
 	public static void main(final String[] args) {
